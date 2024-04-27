07ce1730acb3d2140dacdf23523749245de3fd4d
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-04-27 16:11:06 +0530

    Compilation & Run
    Illustrating the notion of SEQUENCE

--- Lecture 3/Explore.java
+++ Lecture 3/Explore.java
@@ -1,7 +1,8 @@
 public static void main(String[] args) {
     System.out.println("Namaste");
+    System.out.println("Java");
 }
-
+ 
 // Commands for execution
 // java Explore.java
 // java --enable-preview Explore.java
