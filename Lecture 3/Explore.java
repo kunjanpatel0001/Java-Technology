8677b168efe4fa2d9ae90511ba6b6621864b1726
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-04-27 15:56:07 +0530

    Swagatam Program

---
+++ Lecture 3/Explore.java
@@ -0,0 +1,8 @@
+public static void main(String[] args) {
+    System.out.println("Namaste");
+}
+
+// Commands for execution
+// java Explore.java
+// java --enable-preview Explore.java
+// java --source 21 --enable-preview Explore.java
