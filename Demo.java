fb9a9372bf952f466717c7124b70276cc069933c
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-04-29 15:14:58 +0530

    Concept of Final Variable
    It provides a constraint that the variable value can not be changes after it is given their 1st value

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public static void main(String[] args){
+    System.out.println("Namaste");
+    final int i;        //modifier "final" will able to give value to the variable only once
+    i = 10;
+    System.out.println(i);
+    // i = 20;     // will give error
+ }
