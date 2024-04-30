fa12227bac1a4630c4802ba5dd8b1ef5cc7b436d
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-04-30 16:08:23 +0530

    If-Else and String Concatenation

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,18 +1,16 @@
 class Demo {
     public static void main(String[] args){
-    System.out.println("If-Else loop");
-    System.out.println("On Monday it is not raining");
-    goingToMarket(false);
-    System.out.println("On Tuesday it is raining.");
-    goingToMarket(true);
-}
-
-public static void goingToMarket(boolean raining){
-    System.out.println("I am going to market.");
-    if(raining){
-        System.out.println("Carry an umbrella");
-    }
-    System.out.println("Buy fruits and veggies");
-    System.out.println("Return Home.");
-}
+        System.out.println("If-Else & String Concatenation");
+        grader("Rahul",40);
+        grader("Prameet",20);
+     }
+ 
+     public static void grader(String name, int marks){
+        if (marks >= 35) {
+            System.out.println(name + " You have passed");      //String Concatenation
+        }
+        else {
+            System.out.println(name + " You have failed");
+        }
+     }
 }
