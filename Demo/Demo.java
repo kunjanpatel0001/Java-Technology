5c55a8152622e1b5a6c639fc9bcbf0e8030d02c5
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-04-30 16:20:53 +0530

    Import Statement & GUI

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,16 +1,8 @@
+import static javax.swing.JOptionPane.*;
+
+// to avoid writing this big function name: javax.swing.JOptionPane.showMessageDialog(), improt statement is added
 class Demo {
     public static void main(String[] args){
-        System.out.println("If-Else & String Concatenation");
-        grader("Rahul",40);
-        grader("Prameet",20);
-     }
- 
-     public static void grader(String name, int marks){
-        if (marks >= 35) {
-            System.out.println(name + " You have passed");      //String Concatenation
-        }
-        else {
-            System.out.println(name + " You have failed");
-        }
-     }
+        showMessageDialog(null, "Namaste");
+    }
 }
