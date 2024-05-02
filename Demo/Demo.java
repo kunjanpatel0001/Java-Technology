3ef9e5dbcc1d60328c99c01290d441c0f3225bf3
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-02 19:13:58 +0530

    Filter

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,9 +1,12 @@
 class Demo{
     public static void main(String[] args) {
-        System.out.println("For Loop");
+        System.out.println("Filter");
         int[] a = new int[] {10,20,30,40,50};
         for(int i = 0; i < 5; i++){
-          System.out.println(a[i]);
+          if(a[i] > 30){
+            int v = a[i] * 2;
+            System.out.println(v);
+          }
         }
         }
 }
