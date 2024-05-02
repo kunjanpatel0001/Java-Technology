d74f0cc93a3cec3ed2dca00171da9a94102aaae4
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-02 19:56:41 +0530

    Reduce Operation / Folding

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,20 +1,10 @@
 class Demo{
     public static void main(String[] args) {
-        System.out.println("Map Operation");
-        int[] a = new int[] {10,20,30,40,50};
-        int[] b = new int[5];
-        for(int i = 0; i < 5; i++){
-            int v = a[i] * 2;
-            b[i] = v;
-          }
-        System.out.println("Original Array");
-        for(int j=0; j<5; j++){
-            System.out.println(a[j]);
+        System.out.println("Reduce Operation");
+        int[] a = new int[] {2,3,4,5};
+        int sum = 0;
+        for(int i=0; i<4; i++) sum = sum + a[i];
+        System.out.println("Sum = " + sum);
         }
-
-        System.out.println("Resulted Array");
-        for(int k=0; k<5; k++){
-            System.out.println(b[k]);
-        }
-        }
-        }
+    }
+    
