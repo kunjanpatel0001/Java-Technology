16934f98d1cec5b2ba7ca1b897f7209af6f7d26d
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-02 19:19:19 +0530

    Map Operation

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,12 +1,20 @@
 class Demo{
     public static void main(String[] args) {
-        System.out.println("Filter");
+        System.out.println("Map Operation");
         int[] a = new int[] {10,20,30,40,50};
+        int[] b = new int[5];
         for(int i = 0; i < 5; i++){
-          if(a[i] > 30){
             int v = a[i] * 2;
-            System.out.println(v);
+            b[i] = v;
           }
+        System.out.println("Original Array");
+        for(int j=0; j<5; j++){
+            System.out.println(a[j]);
+        }
+
+        System.out.println("Resulted Array");
+        for(int k=0; k<5; k++){
+            System.out.println(b[k]);
         }
         }
-}
+        }
