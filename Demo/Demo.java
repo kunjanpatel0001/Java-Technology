c0f49e62da1b37754b5a5968180ebd717d0a800e
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-02 18:59:45 +0530

    For Loop

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,12 +1,9 @@
 class Demo{
-public static void main(String[] args){
-    System.out.println("While Loop");
-    int[] a = new int[] {10,20,30,40,50};
-    int i = 0;
-    while(i < 5){
-        System.out.println(a[i]);
-        i++;
-    } 
-    
-    }
+    public static void main(String[] args) {
+        System.out.println("For Loop");
+        int[] a = new int[] {10,20,30,40,50};
+        for(int i = 0; i < 5; i++){
+          System.out.println(a[i]);
+        }
+        }
 }
