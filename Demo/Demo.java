71db00ffc13030d039c2fe23bb21c7962eb9b8ca
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-02 18:06:42 +0530

    While Loop

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,10 +1,12 @@
 class Demo{
 public static void main(String[] args){
-    System.out.println("Do While with arrays");
-    int[] a = new int[] {10, 20, 30, 40, 50};
-    int i = 0 ;
-    do{
+    System.out.println("While Loop");
+    int[] a = new int[] {10,20,30,40,50};
+    int i = 0;
+    while(i < 5){
         System.out.println(a[i]);
-    }while (++i < 5);
+        i++;
+    } 
+    
     }
-    }
+}
