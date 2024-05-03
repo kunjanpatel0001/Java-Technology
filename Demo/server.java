bd501e9a6f3a327bf944ff2a5dee0aedbc5a80eb
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-03 21:23:44 +0530

    Networking
    - Server & Client side java files

---
+++ Demo/server.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+
+   public static void main(String[] args) throws IOException {
+       System.out.println("Server Signing ON");
+       ServerSocket ss = new ServerSocket(9081);
+       Socket soc = ss.accept();
+       BufferedReader nis = new BufferedReader(
+                               new InputStreamReader(
+                                       soc.getInputStream()
+                               )
+       );
+       String str = nis.readLine();
+       while(!str.equals("End"))
+       {
+           System.out.println("Server Recived = " + str);
+           str =  nis.readLine();
+       }
+       System.out.println("Server Signing OFF");
+       ss.close();
+   }
+  
+}
