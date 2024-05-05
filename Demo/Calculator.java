71cc7ca881a2272e139e3fcdcf8f533c02b7ff6d
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-05 11:40:23 +0530

    Modified Calculator

--- Demo/Calculator.java
+++ Demo/Calculator.java
@@ -2,20 +2,16 @@ import javax.swing.*;
 
 public class Calculator {
     public static void main(String[] args) {
-        String number1 = JOptionPane.showInputDialog("Enter 1st number: ");
-        String number2 = JOptionPane.showInputDialog("Enter 2nd number: ");
-        String operation = JOptionPane.showInputDialog(
+        Float n1 = Float.parseFloat(JOptionPane.showInputDialog("Enter 1st number: "));
+        Float n2 = Float.parseFloat(JOptionPane.showInputDialog("Enter 2nd number: "));
+        int operation = Integer.parseInt(JOptionPane.showInputDialog(
             "1 : Addition \n" + 
             "2 : Subtraction \n" +
             "3 : Division \n" +
-            "4 : Multiplication");
+            "4 : Multiplication"));
+        float result = 0;
 
-        int n1 = Integer.parseInt(number1);
-        int n2 = Integer.parseInt(number2);
-        int op = Integer.parseInt(operation);
-        int result = 0;
-
-        switch(op){
+        switch(operation){
             case 1: result = n1 + n2;break;
             case 2:result = n1 - n2;break;
             case 3: result = n1 / n2; break;
