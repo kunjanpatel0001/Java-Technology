fe161e70b48894cd08a23329a282b3d6aa81eefe
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-06 16:58:37 +0530

    Modular Counter

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -2,21 +2,40 @@ public class Demo {
     static int count;
     static String CommandSequence = "iiiddiipriiiidp";
    public static void main(String[] args) {
-        System.out.println("Counter");
+        System.out.println("Modular Counter");
         int length = CommandSequence.length();
         for(int j = 0; j < length; j++){
-            if(CommandSequence.charAt(j) == 'i'){
-                count++;
-            }
-            else if(CommandSequence.charAt(j) == 'd'){
-                count--;
-            }
-            else if(CommandSequence.charAt(j) == 'p'){
-                System.out.println(count);
-            }
-            else if(CommandSequence.charAt(j) == 'r'){
-                count = 0;
+            switch(CommandSequence.charAt(j)){
+                case 'i':
+                    Counter.increment();
+                    break;
+                case 'd':
+                    Counter.decrement();
+                    break;
+                case 'p':
+                    Counter.print();
+                    break;
+                case 'r':
+                    Counter.reset();
+                    break;
             }
         }
    } 
+}
+
+class Counter{
+    static int count = 0;
+    static void increment(){
+        count++;
+    }
+
+    static void decrement(){
+        count--;
+    }
+    static void print(){
+        System.out.println(count);
+    }
+    static void reset(){
+        count = 0;
+    }
 }
