51f624d7f547fb87a6a80dc18cb35ea1f070a22f
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-06 15:50:58 +0530

    String : length() & charAt()

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,22 +1,8 @@
 public class Demo {
    public static void main(String[] args) {
-        System.out.println("switch case and break statement ");
-       char ch = 'c';
-        switch(ch){
-            case 'a':
-                System.out.println("ch is a");
-                break;
-            case 'b':
-                System.out.println("ch is b");
-                break;
-            case 'c':
-                System.out.println("ch is c");
-                break;
-            case 'd':
-                System.out.println("ch is d");
-                break;
-            default:
-                System.out.println("default");
-        }
+        System.out.println("String : length() & charAt()");
+        String str = "Hello";
+        System.out.println(str.length());
+        System.out.println(str.charAt(2));
    } 
 }
