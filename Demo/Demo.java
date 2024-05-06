d8753a7ee30adb25211a5543196b4026535494af
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-06 17:20:33 +0530

    Multiple Counter - Module style

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,40 +1,151 @@
 public class Demo {
-    static int count;
-    static String CommandSequence = "iiiddiipriiiidp";
-   public static void main(String[] args) {
-        System.out.println("Counter with Range Checking");
-        System.out.println(Counter.getCount());
+    public static void main(String[] args) {
 
-       Counter.increment();
-
-       Counter.increment();
-
-       System.out.println(Counter.getCount());
-
-       Counter.decrement();
-
-       System.out.println(Counter.getCount());
-   } 
-}
-
-class Counter{
+        System.out.println("Multiple Counter - Module style");
+ 
+ 
+        System.out.println("Counter1 = " + Counter1.getCount());
+ 
+        Counter1.increment();
+ 
+        Counter1.increment();
+ 
+        System.out.println("Counter1 = " + Counter1.getCount());
+ 
+        Counter1.decrement();
+ 
+        System.out.println("Counter1 = " + Counter1.getCount());
+ 
+ 
+        System.out.println("Counter2 = " + Counter2.getCount());
+ 
+        Counter2.increment();
+ 
+        Counter2.increment();
+ 
+        Counter2.increment();
+ 
+        System.out.println("Counter2 = " + Counter2.getCount());
+ 
+        Counter2.decrement();
+ 
+        System.out.println("Counter2 = " + Counter2.getCount());
+ 
+ 
+        System.out.println("Counter3 = " + Counter3.getCount());
+ 
+        Counter3.increment();
+ 
+        Counter3.increment();
+ 
+        System.out.println("Counter3 = " + Counter3.getCount());
+ 
+        Counter3.decrement();
+ 
+        Counter3.decrement();
+ 
+        Counter3.decrement();
+ 
+        System.out.println("Counter3 = " + Counter3.getCount());
+ 
+ 
+    }
+ 
+ 
+ }
+ 
+ 
+ class Counter1 {
+ 
     private static int count;
-    static void increment(){
+ 
+ 
+    static void increment() {
+ 
         if (count < 100)
-        count++;
+ 
+            count++;
+ 
     }
-
-    static void decrement(){
+ 
+ 
+    static void decrement() {
+ 
         if (count > 0)
-        count--;
+ 
+            count--;
+ 
     }
-    static void print(){
-        System.out.println(count);
-    }
-    static void reset(){
-        count = 0;
-    }
-    static int getCount(){
+ 
+ 
+    static int getCount() {
+ 
         return count;
+ 
     }
-}
+ 
+ }
+ 
+ 
+ class Counter2 {
+ 
+    private static int count;
+ 
+ 
+    static void increment() {
+ 
+        if (count < 100)
+ 
+            count++;
+ 
+    }
+ 
+ 
+    static void decrement() {
+ 
+        if (count > 0)
+ 
+            count--;
+ 
+    }
+ 
+ 
+    static int getCount() {
+ 
+        return count;
+ 
+    }
+ 
+ }
+ 
+ 
+ class Counter3 {
+ 
+    private static int count;
+ 
+ 
+    static void increment() {
+ 
+        if (count < 100)
+ 
+            count++;
+ 
+    }
+ 
+ 
+    static void decrement() {
+ 
+        if (count > 0)
+ 
+            count--;
+ 
+    }
+ 
+ 
+    static int getCount() {
+ 
+        return count;
+ 
+    }
+ 
+ }
