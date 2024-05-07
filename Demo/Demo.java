8c80803190fff80e07ba21867bf0e3e3db02fc9e
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-07 20:21:51 +0530

    Experimenting with Static and private

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,151 +1,20 @@
 public class Demo {
     public static void main(String[] args) {
+        System.out.println(C1.a);
+        System.out.println(C2.a);
+        C1.m1();
 
-        System.out.println("Multiple Counter - Module style");
- 
- 
-        System.out.println("Counter1 = " + Counter1.getCount());
- 
-        Counter1.increment();
- 
-        Counter1.increment();
- 
-        System.out.println("Counter1 = " + Counter1.getCount());
- 
-        Counter1.decrement();
- 
-        System.out.println("Counter1 = " + Counter1.getCount());
- 
- 
-        System.out.println("Counter2 = " + Counter2.getCount());
- 
-        Counter2.increment();
- 
-        Counter2.increment();
- 
-        Counter2.increment();
- 
-        System.out.println("Counter2 = " + Counter2.getCount());
- 
-        Counter2.decrement();
- 
-        System.out.println("Counter2 = " + Counter2.getCount());
- 
- 
-        System.out.println("Counter3 = " + Counter3.getCount());
- 
-        Counter3.increment();
- 
-        Counter3.increment();
- 
-        System.out.println("Counter3 = " + Counter3.getCount());
- 
-        Counter3.decrement();
- 
-        Counter3.decrement();
- 
-        Counter3.decrement();
- 
-        System.out.println("Counter3 = " + Counter3.getCount());
- 
- 
+
     }
- 
- 
- }
- 
- 
- class Counter1 {
- 
-    private static int count;
- 
- 
-    static void increment() {
- 
-        if (count < 100)
- 
-            count++;
- 
+    class C1{
+        static int a = 10;
+        static void m1(){
+            System.out.println("m1");
+            System.out.println(a);
+        }
     }
- 
- 
-    static void decrement() {
- 
-        if (count > 0)
- 
-            count--;
- 
+
+    class C2{
+        static int a = 20;
     }
- 
- 
-    static int getCount() {
- 
-        return count;
- 
-    }
- 
- }
- 
- 
- class Counter2 {
- 
-    private static int count;
- 
- 
-    static void increment() {
- 
-        if (count < 100)
- 
-            count++;
- 
-    }
- 
- 
-    static void decrement() {
- 
-        if (count > 0)
- 
-            count--;
- 
-    }
- 
- 
-    static int getCount() {
- 
-        return count;
- 
-    }
- 
- }
- 
- 
- class Counter3 {
- 
-    private static int count;
- 
- 
-    static void increment() {
- 
-        if (count < 100)
- 
-            count++;
- 
-    }
- 
- 
-    static void decrement() {
- 
-        if (count > 0)
- 
-            count--;
- 
-    }
- 
- 
-    static int getCount() {
- 
-        return count;
- 
-    }
- 
- }
+}
