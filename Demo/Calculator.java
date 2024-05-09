3a7265c555d9a098e44b42e46e492c752e368b65
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-09 16:55:00 +0530

    Counter Module
    - static functions
    - private variables

--- Demo/Calculator.java
+++ Demo/Calculator.java
@@ -1,22 +1,140 @@
-import javax.swing.*;
-
 public class Calculator {
-    public static void main(String[] args) {
-        Float n1 = Float.parseFloat(JOptionPane.showInputDialog("Enter 1st number: "));
-        Float n2 = Float.parseFloat(JOptionPane.showInputDialog("Enter 2nd number: "));
-        int operation = Integer.parseInt(JOptionPane.showInputDialog(
-            "1 : Addition \n" + 
-            "2 : Subtraction \n" +
-            "3 : Division \n" +
-            "4 : Multiplication"));
-        float result = 0;
 
-        switch(operation){
-            case 1: result = n1 + n2;break;
-            case 2:result = n1 - n2;break;
-            case 3: result = n1 / n2; break;
-            case 4: result = n1 * n2; break;
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Counter Module");
+
+        Counter.setMax(10);
+
+        Counter.setCyclic(true);
+
+        for (int i = 0; i < 15; i++) {
+
+            Counter.increment();
+
+            Counter.showCounter();
+
         }
-        JOptionPane.showMessageDialog(null, "Result = " + result);
+
+        Counter.setCyclic(false);
+
+        for (int j = 0; j < 12; j++) {
+
+            Counter.decrement();
+
+            Counter.showCounter();
+
+        }
+
+        Counter.setMax(5);
+
+        Counter.setCyclic(true);
+
+        for (int i = 0; i < 15; i++) {
+
+            Counter.increment();
+
+            Counter.showCounter();
+
+        }
+
+        Counter.reset();
+
+        Counter.showCounter();
+
+        System.out.println("The cyclic status is " + Counter.getCyclic());
+
+        System.out.println("The max status is " + Counter.getMax());
+
     }
-}
+
+ }
+
+
+ class Counter {
+
+    private static int count;
+
+    private static boolean cyclic;
+
+    private static int max;
+
+
+    static void increment() {
+
+        if (count < max)
+
+            count++;
+
+        else if (cyclic)
+
+            count = 0;
+
+    }
+
+
+    static void decrement() {
+
+        if (count > 0)
+
+            count--;
+
+        else if (cyclic)
+
+            count = max;
+
+    }
+
+
+    static void showCounter() {
+
+        System.out.println("count = " + count);
+
+        System.out.println("cyclic = " + cyclic);
+
+        System.out.println("max = " + max);
+
+    }
+
+
+    static void setCyclic(boolean flag) {
+
+        cyclic = flag;
+
+    }
+
+
+    static boolean getCyclic() {
+
+        return cyclic;
+
+    }
+
+
+    static void setMax(int value) {
+
+        if (count > value) {
+
+            count = value;
+
+        }
+
+        max = value;
+
+    }
+
+
+    static int getMax() {
+
+        return max;
+
+    }
+
+
+    static void reset() {
+
+        count = 0;
+
+    }
+
+ }
