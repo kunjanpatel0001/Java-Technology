ebccea857553eb6da7669bc7ab02f062bfac4226
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-26 02:57:06 +0530

    Encapsulation

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,44 +1,35 @@
 class Demo{
     public static void main(String[] args){
-        c1 obj1 = new c1();
+        C1 obj1 = new C1();
         obj1.a = 1;
         obj1.b = 2;
-        printc1(obj1);
-        c2 obj2 = new c2();
+        obj1.printC1();C2 obj2 = new C2();
         obj2.a = 10;
         obj2.b = 20;
         obj2.c = 30;
         obj2.d = 40;
-        printc2(obj2);
-        System.out.println("Illustrating Liskov's Principle Of Substitution");
-        System.out.println("Parent Handle can Point to Child Object");
-        System.out.println("Child Object can act as a substitute of Parent Object");
-
-        printc1(obj2);   
-    }
-
-    static void printc1(c1 c){
-        System.out.println(c.a);
-        System.out.println(c.b);
-
-    }
-
-    static void printc2(c2 c){
-        System.out.println(c.a);
-        System.out.println(c.b);
-        System.out.println(c.c);
-        System.out.println(c.d);
-
-
+        obj2.printC2();
+        System.out.println("Parent Method can be Invoked on child Object" );  
+       obj2.printC1();    
     }
 }
 
-class c1{
+class C1{
     int a;
     int b;
+    void printC1(){
+        System.out.println("a = " + this.a);
+        System.out.println("b = " + this.b);
+    }
 }
 
-class c2 extends c1{
+class C2 extends C1{
     int c;
     int d;
+    void printC2(){
+        System.out.println("a = " + this.a);
+        System.out.println("b = " + this.b);
+        System.out.println("c = " + this.c);
+        System.out.println("d = " + this.d);
+    }
 }
