2e18710b120601598f318436e0778424f7b846f6
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-27 23:23:43 +0530

    Data Hiding, super keyword

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,35 +1,43 @@
-class Demo{
-    public static void main(String[] args){
-        C1 obj1 = new C1();
-        obj1.a = 1;
-        obj1.b = 2;
-        obj1.printC1();C2 obj2 = new C2();
-        obj2.a = 10;
-        obj2.b = 20;
-        obj2.c = 30;
-        obj2.d = 40;
-        obj2.printC2();
-        System.out.println("Parent Method can be Invoked on child Object" );  
-       obj2.printC1();    
+class Reusability {
+    public static void main(String[] args) {
+        System.out.println("Data Hiding, super keyword");
+        C1 obj1 = new C1(1,2);
+        obj1.print();
+        C2 obj2 = new C2(10,20,30,40);
+        obj2.print();
     }
 }
 
-class C1{
-    int a;
-    int b;
-    void printC1(){
-        System.out.println("a = " + this.a);
-        System.out.println("b = " + this.b);
-    }
+class C1 {
+    private int a;
+    private int b;
+
+    C1(int a, int b)
+   {
+       this.a = a;
+       this.b = b;
+   }
+   void print()
+   {
+       System.out.println("a = " + this.a);
+       System.out.println("b = " + this.b);
+   }
 }
 
 class C2 extends C1{
     int c;
     int d;
-    void printC2(){
-        System.out.println("a = " + this.a);
-        System.out.println("b = " + this.b);
-        System.out.println("c = " + this.c);
-        System.out.println("d = " + this.d);
+
+    C2(int a, int b, int c, int d){
+        super(a,b);
+        this.c = c;
+        this.d = d;
     }
+    void print()
+   {
+       super.print();
+       System.out.println("c = " + this.c);
+       System.out.println("d = " + this.d);
+   }
+
 }
