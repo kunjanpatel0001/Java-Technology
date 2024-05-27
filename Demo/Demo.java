c76d584fca1e5218b627e7a0142438fb99e60267
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-28 04:07:10 +0530

    Polymorphism

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -1,43 +1,59 @@
-class Reusability {
-    public static void main(String[] args) {
-        System.out.println("Data Hiding, super keyword");
-        C1 obj1 = new C1(1,2);
+
+class Demo{
+    public static void main(String[] args){
+        System.out.println("Polymorphism");
+        C1 obj1 = new C1(1, 2);
         obj1.print();
         C2 obj2 = new C2(10,20,30,40);
         obj2.print();
+        stage(obj1);        //C1 sing
+        stage(obj2);        // C2 sing
+    }
+
+    static void stage(C1 obj){
+        obj.sing();
     }
 }
 
-class C1 {
+class C1{
     private int a;
     private int b;
 
-    C1(int a, int b)
-   {
-       this.a = a;
-       this.b = b;
-   }
-   void print()
-   {
-       System.out.println("a = " + this.a);
-       System.out.println("b = " + this.b);
-   }
+    void print(){
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+
+    C1(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    
+    void sing(){
+        System.out.println("C1 sing");
+    }
 }
 
 class C2 extends C1{
-    int c;
-    int d;
+    private int c;
+    private int d;
 
     C2(int a, int b, int c, int d){
-        super(a,b);
-        this.c = c;
-        this.d = d;
+    super(a, b);
+    this.c = c;
+    this.d = d;
     }
-    void print()
-   {
-       super.print();
-       System.out.println("c = " + this.c);
-       System.out.println("d = " + this.d);
-   }
+
+    void print(){
+        super.print();
+        System.out.println("c: "+ c);
+        System.out.println("d: "+ d);
+
+    }
+
+    @Override
+    void sing(){
+        System.out.println("C2 sing");
+    }
 
 }
