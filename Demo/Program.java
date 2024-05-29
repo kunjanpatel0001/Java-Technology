f13fa245be4e877fc12c8b43e9437f53641c9132
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-30 04:01:37 +0530

    Polymorphism, Inheritance, Abstract, Interface, implements, Liskov's principle

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,68 +1,74 @@
 public class Program {
     public static void main(String[] args) {
-        GreenPiggy client1 = new GreenPiggy();
-        RedPiggy client2 = new RedPiggy();
-        GreenClient(client1);
-        RedClient(client2);
+    System.out.println("Polymorphism, Inheritance, Abstract, Interface, implements, Liskov's principle");
+    GreenPiggy pg1 = new GreenPiggy();
+    RedPiggy pg2 = new RedPiggy();
+    Client(pg1);
+    Client(pg2);
+}
+
+static void Client(Piggy pg) {
+    pg.deposit(200);
+    pg.deposit(40);
+    pg.withdraw(33);
+    pg.statement();
+}  
+}
+
+interface Piggy {
+void deposit(int v);
+void withdraw(int v);
+void statement();
+}
+
+class GreenPiggy implements Piggy {
+int balance;
+int lt;
+
+@Override
+public void deposit(int v) {
+    balance = balance + v;
+    lt = v;
+}
+
+@Override
+public void withdraw(int v) {
+    if (balance >= v) {
+        balance = balance - v;
+        lt = -v;
     }
+}
 
-    static void GreenClient(GreenPiggy client) {
-        client.deposit(200);
-        client.deposit(40);
-        client.withdraw(33);
-        client.statement();
-    }
+@Override
+public void statement() {
+    System.out.println("Printing Statement");
+    System.out.println("Balance = " + balance);
+    System.out.println("Last Transaction = " + lt);
+}
+}
 
-    static void RedClient(RedPiggy client) {
-        client.deposit(200);
-        client.deposit(40);
-        client.withdraw(33);
-        client.statement();
-    }
- }
+class RedPiggy implements Piggy {
+int currentbalance;
+int previousbalance;
 
- class GreenPiggy {
-    int balance;
-    int lt;
+@Override
+public void deposit(int v) {
+    previousbalance = currentbalance;
+    currentbalance = currentbalance + v;
+}
 
-    public void deposit(int v) {
-        balance = balance + v;
-        lt = v;
-    }
-
-    public void withdraw(int v) {
-        if (balance >= v) {
-            balance = balance - v;
-            lt = -v;
-        }
-    }
-
-    public void statement() {
-        System.out.println("Printing Statement");
-        System.out.println("Balance = " + balance);
-        System.out.println("Last Transaction = " + lt);
-    }
- }
-
- class RedPiggy {
-    int currentbalance;
-    int previousbalance;
-
-    public void deposit(int v) {
+@Override
+public void withdraw(int v) {
+    if (currentbalance >= v) {
         previousbalance = currentbalance;
-        currentbalance = currentbalance + v;
+        currentbalance = currentbalance - v;
     }
+}
+@Override
+   public void statement() {
+       System.out.println("Printing Statement");
+       System.out.println("Balance = " + currentbalance);
+       System.out.println("Last Transaction = " + (currentbalance - previousbalance));
+   }
+}
 
-    public void withdraw(int v) {
-        if (currentbalance >= v) {
-            previousbalance = currentbalance;
-            currentbalance = currentbalance - v;
-        }
-    }
-
-    public void statement() {
-        System.out.println("Printing Statement");
-        System.out.println("Balance = " + currentbalance);
-        System.out.println("Last Transaction = " + (currentbalance - previousbalance));
-    }
- }
