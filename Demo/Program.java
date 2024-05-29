34ef48b579539ac2b3856f109d5d4540e041248d
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-05-29 22:38:07 +0530

    Green and Red Piggy Bank

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,29 +1,68 @@
 public class Program {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         GreenPiggy client1 = new GreenPiggy();
-        client1.deposit(200);
-        client1.deposit(40);
-        client1.withdraw(33);
-        client1.statement();
-
+        RedPiggy client2 = new RedPiggy();
+        GreenClient(client1);
+        RedClient(client2);
     }
-}
-class GreenPiggy{
+
+    static void GreenClient(GreenPiggy client) {
+        client.deposit(200);
+        client.deposit(40);
+        client.withdraw(33);
+        client.statement();
+    }
+
+    static void RedClient(RedPiggy client) {
+        client.deposit(200);
+        client.deposit(40);
+        client.withdraw(33);
+        client.statement();
+    }
+ }
+
+ class GreenPiggy {
     int balance;
     int lt;
-    public void deposit(int v){
+
+    public void deposit(int v) {
         balance = balance + v;
         lt = v;
     }
 
-    public void withdraw(int v){
-        balance = balance - v;
-        lt = -v;
+    public void withdraw(int v) {
+        if (balance >= v) {
+            balance = balance - v;
+            lt = -v;
+        }
     }
 
-    public void statement(){
+    public void statement() {
         System.out.println("Printing Statement");
         System.out.println("Balance = " + balance);
-        System.out.println("Last Trasaction = " + lt);
+        System.out.println("Last Transaction = " + lt);
     }
-}
+ }
+
+ class RedPiggy {
+    int currentbalance;
+    int previousbalance;
+
+    public void deposit(int v) {
+        previousbalance = currentbalance;
+        currentbalance = currentbalance + v;
+    }
+
+    public void withdraw(int v) {
+        if (currentbalance >= v) {
+            previousbalance = currentbalance;
+            currentbalance = currentbalance - v;
+        }
+    }
+
+    public void statement() {
+        System.out.println("Printing Statement");
+        System.out.println("Balance = " + currentbalance);
+        System.out.println("Last Transaction = " + (currentbalance - previousbalance));
+    }
+ }
