3f56c78474a5ab3392c6f828d46c2bb086a17fd4
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-10 02:22:46 +0530

    Default Method/ Template method pattern

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,81 +1,71 @@
 public class Program {
     public static void main(String[] args) {
-    System.out.println("Polymorphism, Inheritance, Abstract, Interface, implements, Liskov's principle");
-    GreenPiggy pg1 = new GreenPiggy();
-    RedPiggy pg2 = new RedPiggy();
-    Client(pg1);
-    Client(pg2);
-}
-
-static void Client(Piggy pg) {
-    pg.deposit(200);
-    pg.deposit(40);
-    pg.withdraw(33);
-    pg.statement();
-    pg.transaction('d', 44);
-    pg.transaction('w', 22);
-    pg.statement();
-
-}  
-}
-
-interface Piggy {
-void deposit(int v);
-void withdraw(int v);
-void statement();
-void transaction(char op, int v);
-}
-
-class GreenPiggy implements Piggy {
-int balance;
-int lt;
-
-@Override
-public void deposit(int v) {
-    balance = balance + v;
-    lt = v;
-}
-
-@Override
-public void withdraw(int v) {
-    if (balance >= v) {
-        balance = balance - v;
-        lt = -v;
+        System.out.println("Default Method/ Template method pattern ");
+        GreenPiggy pg1 = new GreenPiggy();
+        RedPiggy pg2 = new RedPiggy();
+        Client(pg1);
+        Client(pg2);
     }
-}
-
-@Override
-public void statement() {
-    System.out.println("Printing Statement");
-    System.out.println("Balance = " + balance);
-    System.out.println("Last Transaction = " + lt);
-}
-
-@Override
-public void transaction(char op, int v){
-    switch(op){
-        case 'd':
-            deposit(v);
-            break;
-        case 'w':
-            withdraw(v);
-            break;
-        default:
-            System.err.println("Incorrect Operation in Transaction");
+    static void Client(Piggy pg) {
+        pg.deposit(200);
+        pg.deposit(40);
+        pg.withdraw(33);
+        pg.statement();
+        pg.transaction('d', 44);
+        pg.transaction('w', 22);
+        pg.statement();
     }
-    }
-}
+ }
 
-class RedPiggy implements Piggy {
+ interface Piggy {
+    void deposit(int v);
+    void withdraw(int v);
+    void statement();
+    default void transaction(char op, int v) {
+        switch (op) {
+            case 'd':
+                deposit(v);
+                break;
+            case 'w':
+                withdraw(v);
+                break;
+            default:
+                System.out.println("Incorrect Operation in Transaction");
+        }
+    }
+ }
+
+ class GreenPiggy implements Piggy {
+    int balance;
+    int lt;
+    @Override
+    public void deposit(int v) {
+        balance = balance + v;
+        lt = v;
+    }
+    @Override
+    public void withdraw(int v) {
+        if (balance >= v) {
+            balance = balance - v;
+            lt = -v;
+        }
+    }
+    @Override
+    public void statement() {
+        System.out.println("Printing Statement");
+        System.out.println("Balance = " + balance);
+        System.out.println("Last Transaction = " + lt);
+    }
+ }
+
+ class RedPiggy implements Piggy {
     int currentbalance;
     int previousbalance;
-
     @Override
     public void deposit(int v) {
         previousbalance = currentbalance;
         currentbalance = currentbalance + v;
     }
-
     @Override
     public void withdraw(int v) {
         if (currentbalance >= v) {
@@ -89,19 +79,4 @@ class RedPiggy implements Piggy {
         System.out.println("Balance = " + currentbalance);
         System.out.println("Last Transaction = " + (currentbalance - previousbalance));
     }
-
-    @Override
-    public void transaction(char op, int v){
-        switch(op){
-                case 'd':
-                    deposit(v);
-                    break;
-                case 'w':
-                    withdraw(v);
-                    break;
-                default:
-                    System.err.println("Incorrect Operation in Transaction");
-        }
-    }
-}
-
+ }
