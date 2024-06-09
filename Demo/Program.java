d98c1f14069257ce143b9f9a0561b2b6f5638eab
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-10 03:01:37 +0530

    Enumeration

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,9 +1,9 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Default Method/ Template method pattern ");
-        GreenPiggy pg1 = new GreenPiggy();
+        System.out.println("Enumeration");
+        GreenPiggy pg1 = new GreenPiggy();      //creating an instance p1
         RedPiggy pg2 = new RedPiggy();
-        Client(pg1);
+        Client(pg1);        // calling client method with Green Piggy instance
         Client(pg2);
     }
     static void Client(Piggy pg) {
@@ -11,22 +11,25 @@ public class Program {
         pg.deposit(40);
         pg.withdraw(33);
         pg.statement();
-        pg.transaction('d', 44);
-        pg.transaction('w', 22);
+        pg.transaction(Piggy.PiggyOps.Deposit, 44);
+        pg.transaction(Piggy.PiggyOps.Withdraw, 22);
         pg.statement();
     }
  }
-
+ 
  interface Piggy {
+    enum PiggyOps {
+        Deposit, Withdraw
+    };
     void deposit(int v);
     void withdraw(int v);
     void statement();
-    default void transaction(char op, int v) {
+    default void transaction(PiggyOps op, int v) {
         switch (op) {
-            case 'd':
+            case Deposit:
                 deposit(v);
                 break;
-            case 'w':
+            case Withdraw :
                 withdraw(v);
                 break;
             default:
@@ -34,7 +37,7 @@ public class Program {
         }
     }
  }
-
+ 
  class GreenPiggy implements Piggy {
     int balance;
     int lt;
@@ -57,7 +60,7 @@ public class Program {
         System.out.println("Last Transaction = " + lt);
     }
  }
-
+ 
  class RedPiggy implements Piggy {
     int currentbalance;
     int previousbalance;
