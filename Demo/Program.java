da606b77ef67256135183f498e120047f199f159
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-12 16:14:39 +0530

    General catch

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,8 +1,8 @@
+//ArithmeticException & ArrayIndexOutOfBoundsException both the errors are children of Runtimeexception class and hence can be generalized
+
 public class Program {
-
-
     public static void main(String[] args) {
-        System.out.println("Multiple catch");
+        System.out.println("General catch");
         System.out.println("Entering Main ");
         C1.m1();
         System.out.println("Leaving main");
@@ -18,7 +18,7 @@ public class Program {
  }
  
  class C2 {
-    static boolean flag1 ;
+    static boolean flag1 = true;
     static boolean flag2 ;
     public static void m2() {
         System.out.println("Entering  m2 ");
@@ -29,17 +29,14 @@ public class Program {
                a = b / c;
             }
             System.out.println("Median");
-
             if (flag2) {
                int[] b = new int[10];
                b[19] = 22;
            }
            System.out.println("Leaving try");
-       } catch (ArithmeticException e) {
-           System.out.println("Arithmetic Exception caught");
-       } catch (ArrayIndexOutOfBoundsException a) {
-           System.out.println("ArrayIndexOutOfBoundsException caught");
-       }
+       } catch (RuntimeException e) {
+           System.out.println("Exception caught");
+       } 
        System.out.println("Leaving  m2 ");
    }
 }
