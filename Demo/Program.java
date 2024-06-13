c8c5e13c2e926bda0315d85114ecefeff2f297dd
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-13 15:33:34 +0530

    Exception Handling - finally

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,42 +1,52 @@
-//ArithmeticException & ArrayIndexOutOfBoundsException both the errors are children of Runtimeexception class and hence can be generalized
-
 public class Program {
     public static void main(String[] args) {
-        System.out.println("General catch");
+        System.out.println("Exception Handling - finally"); 
         System.out.println("Entering Main ");
         C1.m1();
         System.out.println("Leaving main");
     }
  }
  
- class C1 {
+class C1 {
     public static void m1() {
         System.out.println("Entering  m1 ");
-        C2.m2();
+        try {
+            System.out.println("Entering try of m1");
+            C2.m2();
+            System.out.println("Leaving try of m1");
+        } catch (ArrayIndexOutOfBoundsException e) { 
+            System.out.println("ArrayIndexOutOfBoundsException caught");
+        } finally {
+            System.out.println("finally of m1");
+        }
         System.out.println("Leaving  m1 ");
     }
  }
  
- class C2 {
-    static boolean flag1 = true;
-    static boolean flag2 ;
+ class C2 {  
+    static boolean flag1 = false;
+    static boolean flag2 = true;
+
     public static void m2() {
         System.out.println("Entering  m2 ");
         try {
-            System.out.println("Entering try");
+            System.out.println("Entering try of m2");
             if (flag1) {
-               int a = 1, b = 2, c = 0;
-               a = b / c;
+                int a = 1, b = 2, c = 0;
+                a = b / c;
             }
-            System.out.println("Median");
+            System.out.println("Median of m2");
             if (flag2) {
-               int[] b = new int[10];
-               b[19] = 22;
-           }
-           System.out.println("Leaving try");
-       } catch (RuntimeException e) {
-           System.out.println("Exception caught");
-       } 
-       System.out.println("Leaving  m2 ");
-   }
-}
+                int[] b = new int[10];
+                b[19] = 22;
+            }
+            System.out.println("Leaving try of m2");
+        } catch (ArithmeticException e) {
+            System.out.println("Exception Caught");
+        } finally {
+            System.out.println("finally of m2");
+        }
+        System.out.println("Leaving  m2 ");
+    }
+ 
+ }
