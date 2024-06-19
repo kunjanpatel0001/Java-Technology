64464abc25719a783c1985237bbece7331790cb2
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-19 16:06:03 +0530

    User Defined exception throwing

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,21 +1,29 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Exception Handling - finally"); 
-        System.out.println("Entering Main ");
+        System.out.println("Userdefined Exception Handling");
+        System.out.println("Entering Main "); 
         C1.m1();
+ 
         System.out.println("Leaving main");
+ 
     }
+ 
+ 
  }
  
-class C1 {
-    public static void m1() {
+ 
+ class C1 {
+    public static void m1() { 
         System.out.println("Entering  m1 ");
-        try {
+        try { 
             System.out.println("Entering try of m1");
             C2.m2();
-            System.out.println("Leaving try of m1");
-        } catch (ArrayIndexOutOfBoundsException e) { 
-            System.out.println("ArrayIndexOutOfBoundsException caught");
+            System.out.println("Leaving try of m1"); 
+        } catch (E2 e) {
+            System.out.println("E2 caught with error " +
+ 
+                                            e.getErrorInfo());
+ 
         } finally {
             System.out.println("finally of m1");
         }
@@ -23,30 +31,62 @@ class C1 {
     }
  }
  
- class C2 {  
-    static boolean flag1 = false;
+ class C2 { 
+    static boolean flag1 = true; 
     static boolean flag2 = true;
 
-    public static void m2() {
-        System.out.println("Entering  m2 ");
+    public static void m2() { 
+        System.out.println("Entering  m2 "); 
         try {
             System.out.println("Entering try of m2");
             if (flag1) {
-                int a = 1, b = 2, c = 0;
-                a = b / c;
+                E1 e1 = new E1("error1"); 
+                throw e1; 
             }
-            System.out.println("Median of m2");
+ 
+            System.out.println("Median of first try");
+            
             if (flag2) {
-                int[] b = new int[10];
-                b[19] = 22;
+                throw new E2("error2");
             }
-            System.out.println("Leaving try of m2");
-        } catch (ArithmeticException e) {
-            System.out.println("Exception Caught");
-        } finally {
-            System.out.println("finally of m2");
-        }
-        System.out.println("Leaving  m2 ");
+            System.out.println("Leaving   try of m2");
+ 
+        } catch (E1 e) { 
+            System.out.println("E1 Caught with error " + e.getErrorInfo()); 
+        } finally { 
+            System.out.println("finally of m2"); 
+        } 
+        System.out.println("Leaving  m2 "); 
+    }
+ }
+ 
+ 
+ class E1 extends RuntimeException { 
+    private String errorInfo; 
+    public String getErrorInfo() {
+        return errorInfo;
+    }
+    public void setErrorInfo(String errorInfo) { 
+        this.errorInfo = errorInfo;
+    }
+    public E1(String errorInfo) {
+        this.errorInfo = errorInfo;
+    }
+ }
+ 
+ class E2 extends RuntimeException {
+ 
+    private String errorInfo;
+ 
+    public String getErrorInfo() {
+        return errorInfo; 
     }
  
+    public void setErrorInfo(String errorInfo) {
+        this.errorInfo = errorInfo;
+    }
+ 
+    public E2(String errorInfo) {
+        this.errorInfo = errorInfo;
+    }
  }
