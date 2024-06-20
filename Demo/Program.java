0ec390c8c1c006f5d75f5fcc8a75de2123b374ea
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-20 16:19:13 +0530

    Java I/O Programming: Summation

--- Demo/Program.java
+++ Demo/Program.java
@@ -2,11 +2,18 @@ import java.io.*;
 
 public class Program {
     public static void main(String[] args) throws Exception{
-        System.out.println("Welcome to JavaIO");
+        System.out.println("JavaIO : Summation");
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
-        System.out.println("Enter your name: ");
-        String str = br.readLine();
-        System.out.println("Welcome to " + str);
+        System.out.println("Enter number 1: ");
+        String str1 = br.readLine();
+        System.out.println("Enter number: 2 ");
+        String str2 = br.readLine();
+        int no1 = Integer.parseInt(str1);
+        int no2 = Integer.parseInt(str2);
+
+        int sum = no1 + no2;
+
+        System.out.println("Sum = " + sum);
     }
 }
