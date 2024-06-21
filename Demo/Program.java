e8bbcca9c64881cb6e5b25d432cfb5f22214a3de
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-21 16:17:09 +0530

    ArrayList - exceeding initial limit

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,30 +1,27 @@
 import java.util.ArrayList;
 
-
 public class Program 
 {
-
-   public static void main(String[] args) 
+ public static void main(String[] args) 
    {
-       System.out.println("ArrayList - exceeding initial limit");
-       System.out.println("We can pass the Initial"+
-                          "Capacity of ArrayList through constructor");
-       ArrayList<Object> a = new ArrayList<Object>(5);
+       System.out.println("ArrayList - using an existing arrayList to create a new one");
+       ArrayList<Object> a = new ArrayList<>(5);
        a.add(10);
        a.add(20);
        a.add(30);
        a.add(40);
        a.add(50);
-       System.out.println(a);
-       System.out.println("ArrayList will increase in it's capacity"+
-                          " as we add more elements");
-       a.add(60);
-       a.add(70);
-       a.add(80);
-       a.add(90);
-       a.add(100);
-       System.out.println("Let us print the array now");
-       System.out.println(a);
-   }
-   
+       System.out.println("a = " + a);
+       System.out.println("We can create an ArrayList from an existing arraylist *a* using this constructor");
+       ArrayList<Object> b = new ArrayList<>(a);
+       System.out.println("It copies the data of arraylist *a*");
+       System.out.println("b = " + b);      
+       System.out.println("We can also add more elements");
+       b.add(100);
+       b.add(200);
+       b.add(300);
+       b.add(400);
+       b.add(500);
+       System.out.println("b = "+ b);
+   }   
 }
