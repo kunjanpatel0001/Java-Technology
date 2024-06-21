0011488676498741c3a3496622ec6e10d6378fbe
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-21 16:03:26 +0530

    ArrayList - exceeding initial limit

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,24 +1,30 @@
 import java.util.ArrayList;
 
+
 public class Program 
 {
+
    public static void main(String[] args) 
    {
-       System.out.println("ArrayList");
-       System.out.println("Let Create an ArrayList");
-       ArrayList<Object> a = new ArrayList<Object>();
-       System.out.println("Initial Capacity of array list is 10");
+       System.out.println("ArrayList - exceeding initial limit");
+       System.out.println("We can pass the Initial"+
+                          "Capacity of ArrayList through constructor");
+       ArrayList<Object> a = new ArrayList<Object>(5);
        a.add(10);
        a.add(20);
        a.add(30);
        a.add(40);
        a.add(50);
+       System.out.println(a);
+       System.out.println("ArrayList will increase in it's capacity"+
+                          " as we add more elements");
        a.add(60);
        a.add(70);
        a.add(80);
        a.add(90);
        a.add(100);
-       System.out.println("Let us print the array list now");
+       System.out.println("Let us print the array now");
        System.out.println(a);
-   }    
+   }
+   
 }
