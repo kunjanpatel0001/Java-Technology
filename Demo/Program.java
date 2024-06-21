7231fbf1a4c35352cb44f539c05f562414e3a3e7
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-21 18:55:06 +0530

    ArrayList - add, indexOf, get, contains, remove, sort

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,46 +1,94 @@
 import java.util.ArrayList;
-
 import java.util.Collections;
 
-
 public class Program {
+    public static void main(String[] args) {
+
+        System.out.println("ArrayList - add, indexOf, get, contains, remove, sort");
+        System.out.println("Array List is one of the collections api .. provided by java");
+        
+        ArrayList<Object> al = new ArrayList<>();
+
+        System.out.println("adding element to array list");
+        al.add("This a String");
+        al.add(10);
+        al.add(23.6);
+        al.add('d');
+
+        System.out.println("\nprinting the arrayList .. ");
+
+        System.out.println(al);
+
+        System.out.println("\nobtaining the contents from the arraylist at a particular index..");
+
+        String s = (String) al.get(0);      //typecasting the content obtained from the array list.
+
+        System.out.println("s=" + s);
+
+        int indexno = al.indexOf(23.6);
+
+        System.out.println("\nthe number 23.6 is located at index no " + indexno);
+
+        System.out.println("\ncreating an arraylist of String which allows only string to be stored ..");
+
+        ArrayList<String> fruitList = new ArrayList<>();
 
 
-   public static void main(String[] args) {
+        fruitList.add("Banana");
 
-        System.out.println("ArrayList - adding, indexOf, get")
+        fruitList.add("Orange");
 
-       System.out.println("Array List is one of the collections api .. provided by java");
+        fruitList.add("Watermelon");
 
-       ArrayList<Object> al = new ArrayList<>();
+        fruitList.add("aple");
 
-       
+        fruitList.add("Carrot");
 
-       System.out.println("adding element to array list");
 
-       al.add("This a String");
+        System.out.println("\nprinting the fruitList .. ");
 
-       al.add(10);
+        System.out.println(fruitList);
 
-       al.add(23.6);
 
-       al.add('d');
+        System.out.println("is Tomatoes present in fruitList " + fruitList.contains("Tomatoes"));
 
-               
+        System.out.println("is Orange present on the fruitList ? " + fruitList.contains("Orange"));
 
-       System.out.println("\nprinting the arrayList .. ");
 
-       System.out.println(al);
+        System.out.println("it Seems Carrot is not a fruit .. lets remove it from the list");
 
-       System.out.println("\nobtaining the contents from the arraylist at a particular index..");
+        fruitList.remove("Carrot");
 
-       String s = (String) al.get(0); //typecasting the content obtained from the array list.
 
-       System.out.println("s=" + s);
+        System.out.println("\nprinting the fruitList .. ");
 
-       int indexno = al.indexOf(23.6);
+        System.out.println(fruitList);
 
-       System.out.println("\nthe number 23.6 is located at index no " + indexno);
-   }
-}
 
+        System.out.println("seems we have spelled 'apple' wrongly ..."+
+
+                            "so lets replace it with correct one");
+
+        indexno = fruitList.indexOf("aple");
+
+        fruitList.set(indexno, "Apple");
+
+
+        System.out.println("\nprinting the fruitList .. ");
+
+        System.out.println(fruitList);
+
+
+        System.out.println("let's try and sort this fruitList ..");
+
+        Collections.sort(fruitList);
+
+
+        System.out.println("\nprinting the fruitListList .. ");
+
+        System.out.println(fruitList);
+
+    }
+
+
+    }
