328aeb8c34331cd6fd0412f6937009dcc5da9447
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-22 17:49:37 +0530

    File Writing

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,94 +1,46 @@
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 
-public class Program {
-    public static void main(String[] args) {
+public class Program{
 
-        System.out.println("ArrayList - add, indexOf, get, contains, remove, sort");
-        System.out.println("Array List is one of the collections api .. provided by java");
-        
-        ArrayList<Object> al = new ArrayList<>();
+   public static void main(String[] args) throws Exception {
 
-        System.out.println("adding element to array list");
-        al.add("This a String");
-        al.add(10);
-        al.add(23.6);
-        al.add('d');
+       System.out.println("JavaIO - File Writing");
 
-        System.out.println("\nprinting the arrayList .. ");
+       BufferedReader kin = new BufferedReader(
+               new InputStreamReader(System.in)
+       );
 
-        System.out.println(al);
+       System.out.println("Type \"End\" to stop entering data?");
 
-        System.out.println("\nobtaining the contents from the arraylist at a particular index..");
+       ArrayList<String> al = new ArrayList<String>();
+       System.out.print("Enter String --> ");
+       String str = kin.readLine();
+       while (!str.equals("End")) {
+           al.add(str);
+           System.out.print("Enter String --> ");
+           str = kin.readLine();
+       }
 
-        String s = (String) al.get(0);      //typecasting the content obtained from the array list.
+       FileWriter fw = new FileWriter("output.txt");
+       BufferedWriter bw = new BufferedWriter(fw);
+       PrintWriter pw = new PrintWriter(bw, true);
 
-        System.out.println("s=" + s);
+       System.out.println("Printing Strings:");
 
-        int indexno = al.indexOf(23.6);
+       for (String s : al) {
+           System.out.println(s);
+           pw.println(s);
+       }
 
-        System.out.println("\nthe number 23.6 is located at index no " + indexno);
+       String max = al.get(0);
+       for (int k = 1; k < al.size(); k++) {
+           if (max.compareTo(al.get(k)) < 0) {
+               max = al.get(k);
+           }
+       }
 
-        System.out.println("\ncreating an arraylist of String which allows only string to be stored ..");
-
-        ArrayList<String> fruitList = new ArrayList<>();
-
-
-        fruitList.add("Banana");
-
-        fruitList.add("Orange");
-
-        fruitList.add("Watermelon");
-
-        fruitList.add("aple");
-
-        fruitList.add("Carrot");
-
-
-        System.out.println("\nprinting the fruitList .. ");
-
-        System.out.println(fruitList);
-
-
-        System.out.println("is Tomatoes present in fruitList " + fruitList.contains("Tomatoes"));
-
-        System.out.println("is Orange present on the fruitList ? " + fruitList.contains("Orange"));
-
-
-        System.out.println("it Seems Carrot is not a fruit .. lets remove it from the list");
-
-        fruitList.remove("Carrot");
-
-
-        System.out.println("\nprinting the fruitList .. ");
-
-        System.out.println(fruitList);
-
-
-        System.out.println("seems we have spelled 'apple' wrongly ..."+
-
-                            "so lets replace it with correct one");
-
-        indexno = fruitList.indexOf("aple");
-
-        fruitList.set(indexno, "Apple");
-
-
-        System.out.println("\nprinting the fruitList .. ");
-
-        System.out.println(fruitList);
-
-
-        System.out.println("let's try and sort this fruitList ..");
-
-        Collections.sort(fruitList);
-
-
-        System.out.println("\nprinting the fruitListList .. ");
-
-        System.out.println(fruitList);
-
-    }
-
-
-    }
+       System.out.println("The Highest String is " + max);
+       pw.println("The Highest String is " + max);
+   }
+}
