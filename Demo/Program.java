c3a17a5c5d43387469551f37fbe2806612f8bb0e
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-22 18:31:41 +0530

    File I/O: Writing, Reading, Appending from 2 Inputs

--- Demo/Program.java
+++ Demo/Program.java
@@ -1,11 +1,10 @@
 import java.io.*;
 import java.util.ArrayList;
 
-public class Program{
-
+public class Program {
    public static void main(String[] args) throws Exception {
 
-       System.out.println("JavaIO - Appending File ");
+       System.out.println("File I/O: Writing, Reading, Appending from 2 Inputs.");
 
        BufferedReader kin = new BufferedReader(
                new InputStreamReader(System.in)
@@ -14,21 +13,30 @@ public class Program{
        System.out.println("Type \"End\" to stop entering data?");
 
        ArrayList<String> al = new ArrayList<String>();
-       System.out.print("Enter String --> ");
+       System.out.print("Enter String --> ");               // Input 1- Keyboard
        String str = kin.readLine();
        while (!str.equals("End")) {
            al.add(str);
            System.out.print("Enter String --> ");
            str = kin.readLine();
        }
-
+       System.out.println("out");
+       BufferedReader fis =
+             new BufferedReader(
+                new FileReader("input.txt")     // Input 2 - File
+             );
+      
+       str = fis.readLine();
+       while (str != null) {
+           al.add(str);
+           str = fis.readLine();        // Reading from File
+       }
        PrintWriter pw
            = new PrintWriter(
                   new BufferedWriter(
-                      new FileWriter("output.txt", true)        // true : append to existing Output.txt
+                          new FileWriter("output.txt", true)        // Writing while appending to the file
                   ), true
              );
-
        System.out.println("Printing Strings:");
 
        for (String s : al) {
