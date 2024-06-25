d5368565885f8877e0f562d4cc4febbcbe787076
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-06-25 15:31:08 +0530

    ActionListener -> ConsolePrinting
    upon Button click - "Button clicked" is printed on the console

--- Gradle/src/main/java/com/kunjan/Namaste.java
+++ Gradle/src/main/java/com/kunjan/Namaste.java
@@ -1,21 +1,42 @@
+// ActionListener -> ConsolePrinting
+
 package com.kunjan;
+import java.awt.BorderLayout;
+import java.awt.event.*;
 import javax.swing.*;
-import java.awt.*;
 
 public class Namaste {
+
    public static void main(String[] args) {
-       JFrame f1 = new JFrame("Testing FlowLayout");
-       FlowLayout flow = new FlowLayout();
-       f1.setLayout(flow);
-       JButton b1 = new JButton("Ok");
-       JTextArea ta = new JTextArea(10,10);
-       JTextField tf = new JTextField(10);
-       f1.add(b1,BorderLayout.SOUTH);
+
+       JFrame f1 = new JFrame("RPISE");
+       JButton b1 = new JButton("Send");
+       f1.setSize(400, 400);
+       f1.setLocation(300, 300);        // location of JFrame on the Screen
+       JTextArea ta = new JTextArea(10, 10);
        f1.add(ta);
-       f1.add(tf,BorderLayout.NORTH);
-       f1.setSize(400,400);
+       ta.setEditable(false);       // textarea cannot be editted 
+       JTextField tf = new JTextField(20);
+       JPanel p1 = new JPanel();
+       p1.add(tf);
+       p1.add(b1);
+       f1.add(p1, BorderLayout.SOUTH);
        f1.setVisible(true);
        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+       L1 l1 = new L1();
+       b1.addActionListener(l1);
    }
-  
+
+}
+
+class L1 implements ActionListener {
+
+   @Override            
+   public void actionPerformed(ActionEvent e) {
+
+       System.out.println("Button Clicked \n");
+
+   }
+
 }
