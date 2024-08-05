504e8730acd673d9460b99e6ae21b75c179cdc4d
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-08-05 08:34:10 +0530

    Super Types - Meta Object
    Concepts:
    getClass(), getName(), getSuperclass(), getInterfaces()
    
    Output:
    Obtaining Super Types
    Obtaining Meta Object of str
    str belongs to type java.lang.String
    The Parent is :
    java.lang.Object
    The Interface SuperTypes are :
    java.io.Serializable
    java.lang.Comparable
    java.lang.CharSequence
    java.lang.constant.Constable
    java.lang.constant.ConstantDesc

--- Reflection - Meta Prog/Explore.java
+++ Reflection - Meta Prog/Explore.java
@@ -1,21 +1,22 @@
-public class Explore{
+public class Explore {
 
-   public static void main(String[] args)
 
-   {
-
-       System.out.println("Welcome to LearingReflection");  
-
-       String str = "Hello World";
-
-       System.out.println("Obtaining Meta Object of str");
-
-       Class c = str.getClass();
-
-       System.out.println("str belongs to type " + c.getName());
-
-      
-
-   }
-
-}
+    public static void main(String[] args)
+ 
+    {
+        System.out.println("Obtaining Super Types");  
+        String str = "Hello World";
+        System.out.println("Obtaining Meta Object of str");
+        Class c = str.getClass();
+        System.out.println("str belongs to type " + c.getName());
+        Class s = c.getSuperclass();        // meta object of the parent class
+        System.out.println("The Parent is :");
+        System.out.println(s.getName());
+        Class [] stypes = c.getInterfaces();        // list of meta objects of the interfaces
+        System.out.println("The Interface SuperTypes are :");
+        for(Class i : stypes)
+        {
+            System.out.println(i.getName());
+        }
+    } 
+ }
