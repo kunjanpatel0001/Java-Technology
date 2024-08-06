48108adaa201d523a124d09c90d80cb4193f2a13
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-08-06 07:04:46 +0530

    getDeclaredFields - Meta Programming

--- Reflection - Meta Prog/Explore.java
+++ Reflection - Meta Prog/Explore.java
@@ -1,36 +1,38 @@
-import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 
 
 public class Explore {
 
    public static void main(String[] args) {
 
-   System.out.println("Welcome to Method Object");  
+       System.out.println("Obtaining Fields");  
 
-   String str = "Hello World";
+       String str = "Hello World";
 
-   System.out.println("Obtaining Meta Object of str");
+       System.out.println("Obtaining Meta Object of str");
 
-   Class c = str.getClass();
+       Class c = str.getClass();
 
-   System.out.println("str belongs to type " + c.getName());
+       System.out.println("str belongs to type " + c.getName());
 
-   Class s = c.getSuperclass();
+       Class s = c.getSuperclass();
 
-   System.out.println("The Parent is :");
+       System.out.println("The Parent is :");
 
-   System.out.println(s.getName());
+       System.out.println(s.getName());
 
-   Method[] m1 = c.getDeclaredMethods();        // getDeclaredMethods gives public & private methods
+       Field [] f1 = c.getDeclaredFields();
 
-   System.out.println("The Methods are :");
+       System.out.println("The Fields are :");
 
-   for(var i : m1){
+       for(Field i : f1)
 
-       System.out.println(i.toGenericString());
+       {
 
-   }
+           System.out.println(i.toGenericString() + ";");
 
- }
+       }
+
+  }
 
 }
