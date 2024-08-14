a114f2d161880f672ba314844079f4ee022a2931
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-08-14 16:48:40 +0530

    Request Listener
    web.xml - Entry
    
    ****************************************************
    
    <listener>
    
            <description>RequestListener</description>
    
            <listener-class>com.rajeshpatkar.Listener1</listener-class>
    
    </listener>

---
+++ Filters/demofilter/src/main/java/com/filter/demofilter/Listener1.java
@@ -0,0 +1,29 @@
+package main.java.com.filter.demofilter;
+import javax.servlet.ServletRequestEvent;
+
+import javax.servlet.ServletRequestListener;
+
+@WenListener
+public class Listener1 implements ServletRequestListener {
+
+    static int count = 0;
+
+    @Override
+
+    public void requestDestroyed(ServletRequestEvent sre) {
+
+        System.out.println("Request is about to be destroyed "+ sre.getServletRequest().getAttribute("id"));
+
+    }
+
+    @Override
+
+    public void requestInitialized(ServletRequestEvent sre) {
+
+        System.out.println("Request Object has been created and will be given id "+ count);
+
+        sre.getServletRequest().setAttribute("id",count++);
+
+    }
+
+} 
