f21278d14939f34e3a138e9cc7311cf8caf1f325
Kunjan Patel <kunjanpatel0001@gmail.com>
2024-08-15 18:48:39 +0530

    Step 1 - Simple Login

---
+++ LoginFramework/loginframework/src/main/java/com/example/loginframework/LoginframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginframeworkApplication.class, args);
+	}
+
+}
